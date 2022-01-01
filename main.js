a78c4094c9d198be6d1628fbfd692dd973a41c8c
Saeunn87 <96057748+Saeunn87@users.noreply.github.com>
2022-01-02 06:07:50 +1300

    Add files via upload

--- main.js
+++ main.js
@@ -11,17 +11,17 @@ for (i = 0; i < myNodelist.length; i++) {
 var close = document.getElementsByClassName("close");
 var i;
 for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
+  close[i].onclick = function () {
     var div = this.parentElement;
     div.style.display = "none";
   }
 }
 
 var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
+list.addEventListener('click', function (ev);
+if (ev.target.tagName === 'LI') {
+  ev.target.classList.toggle('checked');
+}
 }, false);
 
 function newElement() {
@@ -43,9 +43,9 @@ function newElement() {
   li.appendChild(span);
 
   for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
+    close[i].onclick = function () {
       var div = this.parentElement;
       div.style.display = "none";
     }
   }
-} 
+}
